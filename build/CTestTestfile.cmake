79ba9b9ab2bd48474bbbabd8abd65666b57547f1
21120326 <doantanqn000@gmail.com>
2024-06-23 15:59:43 +0700

    Remove build folder

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: D:/Self_Practicing/Computer Vision/Learning_OpenCV/EdgeDetector
-# Build directory: D:/Self_Practicing/Computer Vision/Learning_OpenCV/EdgeDetector/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
